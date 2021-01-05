95bd0c47e67d6e9f2623382c28a295af78b26e42
Prakhar Agrawal <fzd.prakhar@gmail.com>
2021-01-05 21:13:08 +0530

    update

--- script.js
+++ script.js
@@ -2,52 +2,46 @@ var buttons=document.getElementsByClassName("key");
 var display=document.getElementById("display");
 
 var operand1=0;
-var operand2=null;
+var operand2="";
 var operator=null;
+var x=null;
 
 for(var i=0; i<buttons.length; i++){
 	buttons[i].addEventListener('click', function(){
 		
 		var val= this.getAttribute('data-value');
 
-		// if(val=="+"){
-		// 	operand1=parseInt(display.textContent());
-		// 	operator="+";
-		// }
-		// else if(val=="-"){
-		// 	operand1=parseInt(display.textContent());
-		// 	operator="-";
-		// }
-		// else if(val=="/"){
-		// 	operand1=parseInt(display.textContent());
-		// 	operator="/";
-		// }
-		// else if(val=="*"){
-		// 	operand1=parseInt(display.textContent());
-		// 	operator="*";
-		// }
-		// else if(val=="%"){
-		// 	operand1=parseInt(display.textContent());
-		// 	operator="*";
-		// }
 
-		if(val=="="){
-			// operand2=parseInt(display.textContent());
-			// console.log("operator2", operand2)
-			// display.innerText=eval(operand1+operator+operand2);
-			// if("%" in display.innerText){
-			// 	display.innerText=eval()
-			// }
-			display.innerText=eval(display.innerText)
+		
+		if(val=="+"||val=="-"||val=="*"||val=="/"||val=="%"){
+			if(val=="%"){
+				operator='*0.01*';
+			}
+			else{
+				operator=val;
+			}
+			operand1=parseFloat(display.textContent);
+			display.textContent="";
 		}
-		else if(val=='AC'){
-			display.innerText=null
+
+		else if(val=="+/-"){
+				operand1=parseFloat(display.textContent);
+				operand1=-1*operand1;
+				display.textContent=operand1;
+		     }
+		else if(val=="="){
+			operand2=parseFloat(display.textContent);
+
+			display.innerText=eval(operand1+operator+operand2);
 		}
+		
+		else if(val=="AC"){
+			display.innerText=null;
+		}
+
 		else{
 			display.innerText+=val;
 		}
-		
-		
 
 	});
 }
